7ad8b358ef03be2c42bf49a3ab58b355fce156e2
Joao Pedro Spinda | Xpert Pack SP <joao.spinda@xpertpack.com.br>
2025-10-15 13:58:54 -0300

    Salvando alterações

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
 using System.Diagnostics;
 using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILanchesRepository _lancheRepository;
+
+        public HomeController(ILanchesRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
         public IActionResult Index()
         {
-            TempData["Nome"] = "Spinda"; // quando usa o TempData você conegue recuperar o valor apenas uma vez, caso atualize a página, ele some
+            //TempData["Nome"] = "Spinda"; // quando usa o TempData você conegue recuperar o valor apenas uma vez, caso atualize a página, ele some
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
 
-            return View();
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
