3273a99dcb9501006978a51a34f97ec7edf3b1c1
Joao Pedro Spinda | Xpert Pack SP <joao.spinda@xpertpack.com.br>
2025-11-17 11:55:53 -0300

    Finalizando o projeto

---
+++ LanchesMac/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -0,0 +1,39 @@
+﻿using LanchesMac.Areas.Admin.Servicos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminGraficoController : Controller
+    {
+        private readonly GraficoVendasService _graficoVendasService;
+
+        public AdminGraficoController(GraficoVendasService graficoVendasService)
+        {
+            _graficoVendasService = graficoVendasService;
+        }
+
+        public JsonResult VendasLanches(int dias)
+        {
+            var lanches = _graficoVendasService.GetVendasLanches(dias);
+            return Json(lanches);
+        }
+
+        public IActionResult Index(int dias)
+        {
+            return View();
+        }
+
+        public IActionResult VendasMensal(int dias)
+        {
+            return View();
+        }
+
+        public IActionResult VendasSemanal(int dias)
+        {
+            return View();
+        }   
+    }
+}
